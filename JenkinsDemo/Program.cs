6847bf607bf8c052311862db9aac66baf516bca1
Karthik Patlolla <Karthik_Patlolla@epam.com>
2020-03-18 10:46:57 +0530

    Add project files.

---
+++ JenkinsDemo/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JenkinsDemo
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(createMessage());
+        }
+
+        public static string createMessage()
+        {
+            return "Jenkins";
+        }
+    }
+}
---
+++ JenkinsTest/UnitTest1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using JenkinsDemo;
+namespace JenkinsTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.AreEqual("Jenkins",Program.createMessage());
+        }
+    }
+}
